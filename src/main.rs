256a1a5d5c3aa82c786a710a3834ecd814beb49d
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-12-10 14:20:38 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+use std::collections::HashMap;
+
 fn main() {
     loop {
         let input_buffer: String = read();
-        println!("{}: command not found", input_buffer);
+        eval(input_buffer);
+    }
+}
+
+trait Command {
+    // fn name(&self) -> &'static str;
+    fn execute(&self) -> Result<(), ()>;
+}
+
+struct Exit;
+
+impl Command for Exit {
+    /*
+    fn name(&self) -> &'static str {
+        "exit"
+    }
+    */
+    fn execute(&self) -> Result<(), ()> {
+        std::process::exit(0);
     }
 }
 
@@ -20,4 +40,15 @@ fn read() -> String {
     // Removes '\n'
     input_buffer.pop();
     input_buffer
+}
+
+fn eval(input: String) {
+    let mut commands: HashMap<String, Box<dyn Command>> = HashMap::new();
+    commands.insert("exit".to_string(), Box::new(Exit));
+
+    if let Some(cmd) = commands.get(&input) {
+        let _ = cmd.execute();
+    } else {
+        println!("{}: command not found", input);
+    }
 }
