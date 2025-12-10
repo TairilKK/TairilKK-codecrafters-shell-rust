2d85acb8bfb65443ec69532cd0bdc402afbf9038
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-12-10 13:46:48 +0100

    xxx

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,13 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input_buffer = String::new();
+    let _ = io::stdin()
+        .read_line(&mut input_buffer)
+        .expect("Cannot read input.");
+
+    input_buffer.pop();
+
+    println!("{}: command not found.", input_buffer);
 }
