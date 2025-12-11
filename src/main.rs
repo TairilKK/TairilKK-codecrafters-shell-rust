3e82d2892266b869ade7628747f666ac1f4ac058
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-12-11 18:30:29 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,30 @@ use std::collections::HashMap;
 
 fn main() {
     loop {
-        let input_buffer: String = read();
-        eval(input_buffer);
+        let (command, args): (String, String) = read();
+        eval(command, args);
     }
 }
 
 trait Command {
-    // fn name(&self) -> &'static str;
-    fn execute(&self) -> Result<(), ()>;
+    fn execute(&self, args: String) -> Result<(), ()>;
+}
+struct Echo;
+impl Command for Echo {
+    fn execute(&self, args: String) -> Result<(), ()> {
+        println!("{}", args);
+        Ok(())
+    }
 }
 
 struct Exit;
-
 impl Command for Exit {
-    /*
-    fn name(&self) -> &'static str {
-        "exit"
-    }
-    */
-    fn execute(&self) -> Result<(), ()> {
+    fn execute(&self, _: String) -> Result<(), ()> {
         std::process::exit(0);
     }
 }
 
-fn read() -> String {
+fn read() -> (String, String) {
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -39,16 +39,21 @@ fn read() -> String {
 
     // Removes '\n'
     input_buffer.pop();
+
     input_buffer
+        .split_once(' ')
+        .map(|(l, r)| (l.to_string(), r.to_string()))
+        .unwrap_or((input_buffer, "".to_string()))
 }
 
-fn eval(input: String) {
+fn eval(command: String, args: String) {
     let mut commands: HashMap<String, Box<dyn Command>> = HashMap::new();
     commands.insert("exit".to_string(), Box::new(Exit));
+    commands.insert("echo".to_string(), Box::new(Echo));
 
-    if let Some(cmd) = commands.get(&input) {
-        let _ = cmd.execute();
+    if let Some(cmd) = commands.get(&command) {
+        let _ = cmd.execute(args);
     } else {
-        println!("{}: command not found", input);
+        println!("{}: command not found", command);
     }
 }
