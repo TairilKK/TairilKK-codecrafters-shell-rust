8f5ed8742ef1d4a0938dd5ab032cbece5f588dd0
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-12-14 10:08:30 +0100

    codecrafters submit [skip ci]

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -6,9 +6,21 @@ pub struct Cd;
 
 impl Command for Cd {
     fn execute(&self, args: String) -> Result<(), ()> {
-        if let Err(_) = env::set_current_dir(&args) {
-            println!("cd: {}: No such file or directory", args);
+        if args.contains('~') {
+            if let Ok(v) = env::var("HOME") {
+                let args = args.replace("~", v.as_str());
+                if let Err(_) = env::set_current_dir(&args) {
+                    println!("cd: {}: No such file or directory", args);
+                }
+            } else {
+                println!("cd: {}: No such file or directory", args);
+            }
+        } else {
+            if let Err(_) = env::set_current_dir(&args) {
+                println!("cd: {}: No such file or directory", args);
+            }
         }
+
         Ok(())
     }
 }
